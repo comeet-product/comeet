f2c225702aa51cdf6a60dc4bc13aa064593f439a
jaewan <ilyjw0@gmail.com>
2025-05-23 19:14:58 +0900

    Hour 완성

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import Title from "@/components/Title";
+import Half from "@/components/TimetableResult/Half";
+import Hour from "@/components/TimetableResult/Hour";
 
 export default function Home() {
   return (
     <div className="p-4 md:p-6">
       <Title>안녕하세요! 메인 제목입니다</Title>
+      <br />
+      <Half />
+      <br />
+      <Hour />
     </div>
   );
 }
--- src/components/TimetableResult/Half.js
+++ src/components/TimetableResult/Half.js
@@ -1,9 +1,11 @@
 export default function Half({ isTop }) {
   return (
     <div className={`w-full h-[17px] border-main ${
-      isTop 
-        ? 'border border-b-[0.3px]' 
-        : 'border border-t-[0.3px]'
+      isTop === undefined
+        ? 'border-[1.3px]'
+        : isTop 
+          ? 'border-[1.3px] border-b-[1px] border-b-main/50' 
+          : 'border-[1.3px] border-t-0'
     }`}>
     </div>
   );
