12c8f759e66368bbef05268d5bdc4566492aeb2d
hhyyn <aeronaer9@gmail.com>
2025-05-23 18:56:53 +0900

    레이아웃 여백 조정

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Title from "@/components/Title";
 
 export default function Home() {
   return (
-    <div className="p-4 md:p-6">
+    <div className="">
       <Title>안녕하세요! 메인 제목입니다</Title>
     </div>
   );
