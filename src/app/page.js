ff2e1f1ae05ff0d68a2092e318cd9e7b0f64c945
hhyyn <aeronaer9@gmail.com>
2025-05-28 15:09:21 +0900

    page.js 수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,5 @@
 import Title from "@/components/Title";
-import Half from "@/components/TimetableResult/Half";
-import Hour from "@/components/TimetableResult/Hour";
-import Day from "@/components/TimetableResult/Day";
-import TimeSelect from "@/components/SelectableTime/TimeSelect";
 import SelectableTime from "@/components/SelectableTime/SelectableTime";
-import Date from "@/components/TimetableResult/Date";
-import Timetable from "@/components/TimetableResult/Timetable";
-import TimeHeader from "@/components/TimetableResult/TimeHeader";
-import Time from "@/components/TimetableResult/Time";
 import TimetableResult from "@/components/TimetableResult/TimetableResult";
 import Button from "@/components/Button";
 
