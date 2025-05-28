83c60ec8e107f4c700f219c5bc3de01a7388af84
yuunisaugust <seoyun041122@gmail.com>
2025-05-28 09:44:29 +0900

    dev 병합

---
+++ src/lib/test/dummyMeetingData.js
@@ -0,0 +1,36 @@
+export const dummyMeetingData = {
+    title: "프론트-백엔드 통합 테스트",
+    dates: ["2025-07-05", "2025-07-06"],
+    selectableTime: {
+        startTime: 900,
+        endTime: 1800,
+    },
+    recommendations: {
+        "2025-07-05": {
+            availableTime: [
+                {
+                    startTime: 900,
+                    length: 2,
+                    count: 3,
+                    availableUsers: [
+                        { user: "예진" },
+                        { user: "재완" },
+                        { user: "기훈" },
+                    ],
+                },
+                // 추가 시간 블록...
+            ],
+        },
+        "2025-07-06": {
+            availableTime: [
+                {
+                    startTime: 1330,
+                    length: 2,
+                    count: 2,
+                    availableUsers: [{ user: "예진" }, { user: "재완" }],
+                },
+                // 추가 시간 블록...
+            ],
+        },
+    },
+};
