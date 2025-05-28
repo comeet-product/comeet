c64275c3f788764e73d171e93f0ec95af1b32a9b
Yejin Han <aeronaer9@gmail.com>
2025-05-28 11:09:45 +0900

    Revert "Revert 11 comeet 10"

---
+++ src/components/TimetableResult/Day.js
@@ -0,0 +1,30 @@
+import Hour from "./Hour";
+import Half from "./Half";
+
+export default function Day({ halfCount, isFirstDay, hasDateHeaderAbove }) {
+  const hourCount = Math.floor(halfCount / 2);
+  const hasExtraHalf = halfCount % 2 === 1;
+
+  return (
+    <div className="flex flex-col">
+      {/* Hour 컴포넌트들 */}
+      {Array.from({ length: hourCount }, (_, index) => (
+        <Hour 
+          key={`hour-${index}`}
+          isFirst={index === 0}
+          isFirstDay={isFirstDay}
+          hasDateHeaderAbove={hasDateHeaderAbove && index === 0}
+        />
+      ))}
+      
+      {/* 홀수개일 때 마지막 Half */}
+      {hasExtraHalf && (
+        <Half 
+          hasHourAbove={hourCount > 0} 
+          isFirstDay={isFirstDay}
+          hasDateHeaderAbove={hasDateHeaderAbove && hourCount === 0}
+        />
+      )}
+    </div>
+  );
+}
