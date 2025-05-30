da338d3c300fa622a861bdf0629470638d69d00b
jaewan <ilyjw0@gmail.com>
2025-05-30 19:55:25 +0900

    테스트를 위한 커밋

--- src/components/TimetableComponent/Timetable.js
+++ src/components/TimetableComponent/Timetable.js
@@ -1,15 +1,17 @@
 import DateHeader from "./DateHeader";
 import DateSelector from "./DateSelector";
 
-export default function Timetable({ dayCount , halfCount, startDate }) {
+export default function Timetable({ dayCount, halfCount, startDate, visibleDayCount }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       <DateHeader 
         dayCount={dayCount} 
+        visibleDayCount={visibleDayCount}
         startDate={startDate}
       />
       <DateSelector 
         dayCount={dayCount} 
+        visibleDayCount={visibleDayCount}
         halfCount={halfCount}
         hasDateHeaderAbove={true}
       />
