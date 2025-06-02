952394665d52529806fcc3f8fb13dcf90f1ddfb7
hhyyn <aeronaer9@gmail.com>
2025-06-02 22:09:46 +0900

    에러 해결

---
+++ src/lib/supabase/getMeeting.js
@@ -0,0 +1,11 @@
+import { supabase } from "../supabase.js";
+
+export async function getMeeting(meetingId) {
+    const { data } = await supabase
+        .from('meetings')
+        .select('*')
+        .eq('id', meetingId)
+        .single();
+    
+    return data;
+} 
